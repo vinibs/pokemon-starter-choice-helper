d76bf66dea2196d41bedb4ef36f6aff433d78f98
Vinicius Baroni Soares <hiviniciusbs@gmail.com>
2024-03-20 17:40:59 -0300

    feat: base method to handle requests

--- adapters/pokemon_api.py
+++ adapters/pokemon_api.py
@@ -1,8 +1,21 @@
-from typing import Tuple
+from typing import Any, Tuple
 from http import HTTPStatus
 import requests
 
 _base_url = 'https://pokeapi.co/api/v2'
+
+def _handle_request(url: str, extract_data: callable) -> Any:
+    response = requests.get(url)
+
+    if response.status_code != HTTPStatus.OK:
+        return ()
+    
+    try:
+        json_data = response.json()
+        return extract_data(json_data)
+    
+    except Exception:
+        return ()
 
 def get_types(pokemon_name: str) -> Tuple[str]:
     url = f'{_base_url}/pokemon/{pokemon_name}'
