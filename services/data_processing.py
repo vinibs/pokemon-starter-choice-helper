fba63db1f83ed91bfeaa0799efd7516005542e2a
Vinicius Baroni Soares <hiviniciusbs@gmail.com>
2024-03-26 20:26:16 -0300

    feat: data_processing service

---
+++ services/data_processing.py
@@ -0,0 +1,9 @@
+from typing import Dict, Tuple
+from adapters import pokemon_api
+
+def get_typed_pokemon_list(generation_number: int) -> Dict[str, Tuple[str]]:
+    species_from_gen = pokemon_api.get_species_from_generation(generation_number)
+
+    return {
+        species: pokemon_api.get_types(species) for species in species_from_gen
+    }
