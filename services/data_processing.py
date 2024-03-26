98031e1138906b06c68ef8556824c9d6a3a62810
Vinicius Baroni Soares <hiviniciusbs@gmail.com>
2024-03-26 20:51:23 -0300

    refactor: use number-rich dictionary for getting typed pokémon list

--- services/data_processing.py
+++ services/data_processing.py
@@ -5,5 +5,5 @@ def get_typed_pokemon_list(generation_number: int) -> Dict[str, Tuple[str]]:
     species_from_gen = pokemon_api.get_species_from_generation(generation_number)
 
     return {
-        species: pokemon_api.get_types(species) for species in species_from_gen
+        name: pokemon_api.get_types(number) for (number, name) in species_from_gen.items()
     }
